4dc78b31767b6a213d5142e5cf8fd2a03bc35698
Ryan Idrogo <cheruvian@gmail.com>
2025-01-04 23:38:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/bin && \
-curl https://raw.githubusercontent.com/cheruvian/node-git/main/dist/cli.js > ~/bin/git && \
-chmod +x ~/bin/git && \
+mkdir -p ~/bin
+curl https://raw.githubusercontent.com/cheruvian/node-git/main/dist/cli.js > ~/bin/git
+chmod +x ~/bin/git
 alias git=~/bin/git
